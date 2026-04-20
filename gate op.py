ef5ed834d592e351d0dab635628f82f571e1125e
skthangadharshni17 <skthangadharshni_bec28@mepcoeng.ac.in>
2026-04-20 12:07:52 +0530

    Add files via upload

---
+++ gate op.py
@@ -0,0 +1,33 @@
+x = 5
+y = 10
+result = x < y
+print(result)
+a = True
+b = False
+result = a and b
+print(result)
+a = True
+b = False
+result = a or b
+print(result)
+a = True
+result = not a
+print(result)
+x = 5
+y = 10
+z = 15
+result = (x < y) and (y < z)
+print(result)
+result = (x > y) or (y < z)
+print(result)
+value = 10
+print(bool(value))
+value = 0
+print(bool(value))
+value = "hello"
+print(bool(value))
+value = ""
+print(bool(value))
+number = 10
+boolean_value = bool(number)
+print(boolean_value)
